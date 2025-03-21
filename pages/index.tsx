e6049081a5667e8dbcf9afdb71d3e90798c6229f
Emmybrown775 <elumezeemma@gmail.com>
2025-03-21 08:53:33 +0100

    fixed up telegram webap init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import TelegramAuth from "./(components)/TelegramAuth";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,10 @@ export default function Home() {
   return (
     <main>
       <TelegramAuth />
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+      />
     </main>
   );
 }
